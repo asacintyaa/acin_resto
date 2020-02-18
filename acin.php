323af52fee2886c444740a9a81bbc9ed754cfd31
acin <asacintya001@gmail.com>
2020-02-18 22:24:14 +0700

    project pertama

---
+++ acin.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="acin2.css">
+    <title>dashboard</title>
+
+</head>
+<body>
+    <header>
+        <div class="main">
+            <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">Menus</a></li>
+            <li><a href="#">About</a></li>
+            </ul>
+        </div>
+        <div class="title">
+            <h1>WELCOME TO SA-RESTO</h1>
+        </div>
+        
+    </header>
+
+
+
+
+
+</body>
+</html>
---
+++ saa.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Resto</title>
+
+  <link rel="stylesheet" href="saa2.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  
+</head>
+<body>
+ 
+    <div class="login">
+    <div class="avatar">
+    <i class="fa fa-user"></i>
+    </div>
+
+    <form action="acin.php" method="post">
+    <h2>Login</h2>
+
+    <div class="box-login">
+    <i class="fas fa-envelope"></i>
+    <input type="text" placeholder="Email">
+    </div>
+
+    <div class="box-login">
+    <i class="fas fa-lock"></i>
+    <input type="Password" value="" id="myInput" placeholder="Password">  
+    </div>
+
+    <button type="submit" class="btn-login">
+      Login
+    </button> 
+</div>
+</form>
+
+
+
+</body>
+</html>
